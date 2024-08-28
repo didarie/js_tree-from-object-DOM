f7860d63ea64a09f675b2c2e602806e046f531c8
Diana Darienko <diana.darienko21@gmail.com>
2024-08-28 15:02:30 +0300

    make build

---
+++ index.cbd23d49.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var o=document.createElement("li");r.appendChild(o),o.textContent=a,"object"==typeof t[a]&&(n.append(r),e(o,t[a]))}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.cbd23d49.js.map
